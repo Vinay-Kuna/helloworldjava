49510432054895cb231d06d852bd451f3a3c8d9f
Vinay-Kuna <75176633+Vinay-Kuna@users.noreply.github.com>
2021-11-14 22:26:47 +0530

    Update helloworldjava.java

--- helloworldjava.java
+++ helloworldjava.java
@@ -1,6 +1,6 @@
 public class helloworldjava {
    public static void main(String[] args) {
-    System.out.println("Hello World from DevOps! CronJob testing in Jenkins");
+    System.out.println("Hello World from DevOps! added git to jenkins web hook for push mechanism");
     }
 }
 
