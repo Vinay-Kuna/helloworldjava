b21f58644bb636e24ff879b412d7bc2f8a29ca7f
Vinay-Kuna <75176633+Vinay-Kuna@users.noreply.github.com>
2021-11-14 22:52:04 +0530

    Update helloworldjava.java

--- helloworldjava.java
+++ helloworldjava.java
@@ -1,6 +1,6 @@
 public class helloworldjava {
    public static void main(String[] args) {
-    System.out.println("Hello World from DevOps! added git to jenkins web hook for push mech");
+    System.out.println("Hello World from DevOps! added git to jenkins web hook");
     }
 }
 
